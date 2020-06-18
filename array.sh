d520bed7e26836bb3a875b61e3b2a63272e32848
Sriram Sreedhar <sriramsreedhar@gmail.com>
2020-06-18 12:12:12 -0400

    Bash Array

---
+++ array.sh
@@ -0,0 +1,6 @@
+NAME=(krishna radha govinda rama hanuman)
+
+echo Name on index0 is ${NAME[0]}
+echo Name on index1 is ${NAME[1]}
+echo Name on index2 is ${NAME[2]}
+echo Name on index3 is ${NAME[3]}
